579858391e04fcb3d32d923714889551fbe78b6a
kohei-minami <min101060anna@gmail.com>
2024-11-12 00:10:15 +0900

    3と5の両方の倍数のときにFizzBuzzと表示されるように修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,5 +1,5 @@
 for (let number = 1; number <= 20; number++) {
-  if (number % 15 === 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log(`FizzBuzz`);
   } else if (number % 3 === 0) {
     console.log(`Fizz`);
